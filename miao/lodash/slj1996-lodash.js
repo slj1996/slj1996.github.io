a11e52f2915cee1e550407deca10e22a75f96bec
sanglongjia <sanglongjia@gmail.com>
2019-07-17 20:57:43 +0800

    zuoye

---
+++ miao/lodash/slj1996-lodash.js
@@ -0,0 +1,63 @@
+var slj1996 = function () {
+    
+    function chunk (array,number) {
+        let arr = [];
+        for (var i = 0 ; i < array.length;i++) {
+            arr.push(array.shift())
+        }
+        return [arr,array];
+    }
+
+
+
+
+
+    return {
+        chunk,
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}      ()
